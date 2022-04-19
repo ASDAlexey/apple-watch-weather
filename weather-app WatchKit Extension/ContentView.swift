d76d951cb68b3539f4928f8db5bc45474cd0b635
alexey popov <alexey.popov@5thkind.com>
2022-04-19 23:59:54 +0300

    Initial Commit

---
+++ weather-app WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weather-app WatchKit Extension
+//
+//  Created by Alexey on 19.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weather-app WatchKit Extension/weather_appApp.swift
@@ -0,0 +1,19 @@
+//
+//  weather_appApp.swift
+//  weather-app WatchKit Extension
+//
+//  Created by Alexey on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
